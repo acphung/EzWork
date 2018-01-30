229fd9783262a0b17c211f64432ba411f70c117d
the7who <35671829+the7who@users.noreply.github.com>
2018-01-29 18:56:04 -0800

    Add files via upload
    Basic functional login page connecting to an established firebase database. Still needs style work, username uniqueness checks, and more -Dylan

---
+++ Login/loginpage.js
@@ -0,0 +1,85 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+    document.getElementById("create_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+	document.getElementById("create_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
+});
+
+function login(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function signup(){
+    document.getElementById("user_div").style.display = "none";
+	document.getElementById("create_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+}
+
+function create_cancel(){
+    document.getElementById("user_div").style.display = "none";
+	document.getElementById("create_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+}
+
+function create(){
+  var userEmail = document.getElementById("create_email_field").value;
+  var userPass = document.getElementById("create_password_field").value;
+  var userID = document.getElementById("create_userid_field").value;
+  // search users database to make sure userID is unique //
+  // code goes here
+  // error and break if not
+  /////////////////////////////////////////////////////////
+  var success = true;
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+	// Handle Errors here.
+	success = false;
+	var errorCode = error.code;
+	var errorMessage = error.message;
+	window.alert("Error : " + errorMessage);
+	});
+  if(success){
+	  // Add new user to database
+	  window.alert("Sucessful account creation? " + userID);
+	  firebase.database().ref('users/' + userID).set({
+			username: userID,
+			email: userEmail,
+			type: "finder"});
+  }
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
