1646c777704ebaf602d4bb5e085a01848503ba08
the7who <35671829+the7who@users.noreply.github.com>
2018-02-07 18:29:46 -0800

    Add files via upload
    Firebase remade in dual form (private vs public profiles). Private indexed by email with . replaced by (, public indexed by username.

--- create_account/create_account.js
+++ create_account/create_account.js
@@ -1,6 +1,5 @@
 function create_cancel(){
 	// redirect user to homepage here //
-	// *********** ADD CODE **********
 	window.location.href = "../Main/EzWork2.html";
 }
 
@@ -12,6 +11,7 @@ function create(){
 	document.getElementById("bad_email").style.display = "none";
 	
   var userEmail = document.getElementById("create_email_field").value;
+  var emailFixed = userEmail.replace('.','(').toLowerCase();
   var userPass = document.getElementById("create_password_field").value;
   var userPass2 = document.getElementById("create_password2_field").value;
   var userID = document.getElementById("create_userid_field").value;
@@ -26,8 +26,9 @@ function create(){
 	  return;
   }
   // search users database to make sure userID is unique //
-  return firebase.database().ref('users/' + userID).once('value', function(snapshot) {
-	var username = (snapshot.val() && snapshot.val().username);
+  try{ // LATER, add code that throws an error if username has a '/'
+  return firebase.database().ref('usernames/' + userID).once('value', function(snapshot) {
+	var username = (snapshot.val());
 	if(username == null){
 		//username is unique//
 		var success = true;
@@ -36,12 +37,13 @@ function create(){
 			return;
 		}
 		firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).then(function() {
-			firebase.database().ref('users/' + userID).set({
+			firebase.database().ref('users/' + emailFixed).set({
 				username: userID,
 				email: userEmail,
 				type: acctype});
+			firebase.database().ref('usernames/' + userID).set({
+				username: userID});
 			//we succeeded so redirect user to homepage here //
-			// ********* ADD CODE *********
 			window.location.href = "../Main/EzWork2.html";
 		}).catch(function(error) {
 		// Handle account creation errors here.
@@ -50,16 +52,26 @@ function create(){
 		// determine if email or password error and display //
 		if(errorCode == "auth/weak-password"){
 			document.getElementById("bad_password").style.display = "block";
+		}else if(false){
+			//reserved for other error codes
 		}else{
 			document.getElementById("bad_email").innerHTML = errorMessage;
 			document.getElementById("bad_email").style.display = "block";
 		}
 		});
+		
+		
 	}else{
 		// username is not unique //
+		document.getElementById("bad_userid").innerHTML = "An account already exists with that UserID.";
 		document.getElementById("bad_userid").style.display = "block";
 	}
   });
+  }catch (error){
+	// Handle bad userid errors here
+	document.getElementById("bad_userid").innerHTML = "UserIDs cannot contain the any of the following symbols: . $ # / [ ]"
+	document.getElementById("bad_userid").style.display = "block";
+  }
 }
 
 function uncheck_employer(){
